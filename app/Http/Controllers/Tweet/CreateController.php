44403e0ea35353f6b33cedc5d3bd954f3afc6336
takeC00 <take.614@icloud.com>
2022-09-17 20:07:38 +0900

    投稿フォーム（view）実装

---
+++ app/Http/Controllers/Tweet/CreateController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Tweet;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Tweet\CreateRequest;
+use Illuminate\Http\Request;
+
+class CreateController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function __invoke(CreateRequest $request)
+    {
+      
+    }
+}
