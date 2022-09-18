f77a5d9d17ed74b517f68b3afee0cdaf6cff7ef1
takeC00 <take.614@icloud.com>
2022-09-18 18:40:40 +0900

    updateページでidに対応する内容を取得

--- app/Http/Controllers/Tweet/Update/IndexController.php
+++ app/Http/Controllers/Tweet/Update/IndexController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Tweet\Update;
 
 use App\Http\Controllers\Controller;
+use App\Models\Tweet;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class IndexController extends Controller
 {
@@ -15,6 +17,12 @@ class IndexController extends Controller
      */
     public function __invoke(Request $request)
     {
-        //
+        $tweetId = (int) $request->route('tweetId');
+        $tweet = Tweet::where('id', $tweetId)->first();
+        if (is_null($tweet)) {
+          throw new NotFoundHttpException('存在しないつぶやきです');
+        }
+        dd($tweet);
     }
+
 }
